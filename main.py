3ffa14ef4721f3e85418009f56d00afdee016e6c
Serena <swarner26@wooster.edu>
2025-02-17 13:59:49 -0500

    added URL class

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import validators 
 
+class URLs:
+    def __init__(self):
+        self.links = {}
+        self.count = 0
+        return
 
-dictionary = {"www.hottogo.com": "www.htg.com", "www.lame.com": "www.l.com"}
+    def shorten(self,URL):
+        url =  "www.myLink.com/" + str(self.count)
+        self.count+=1
+        return url
+        
 
-dictionary["www.hottogo.com"] = "www.htg.com"
+    def store(self,URL):
+        self.links[URL] = self.shorten(URL)
 
-print(dictionary)
+    def printShorten(self,URL):
+        print(self.links[URL])
+
+
+
