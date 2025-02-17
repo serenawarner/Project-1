6a690f73eb8aba570df1a301241c9c24d9d8e642
critterday <tjons26@wooster.edu>
2025-02-17 13:56:53 -0500

    help me lmao

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-import validators 
+# import validators 
 
+state = "0"
 
-dictionary = {"www.hottogo.com": "www.htg.com", "www.lame.com": "www.l.com"}
+while(state != "00"):
+    print("Would you like to: \n\nStore a URl (0), \nStore and return a shortened URL (1), \nlist stored URLs (2), \nDisplay number of stored URLs (3), \nReturn a full URl from a shortened input (4) \n\n")
+    state = input()
+    if(state == "0"):
+        break
+    if(state == "1"):
+        break
+    if(state == "2"):
+        break
+    if(state == "3"):
+        break
+    if(state == "4"):
+        break
+    break
 
-dictionary["www.hottogo.com"] = "www.htg.com"
-
-print(dictionary)
