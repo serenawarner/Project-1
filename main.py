900f1b856ea147c8abb87bc5d1782562f2fd3d36
critterday <tjons26@wooster.edu>
2025-02-19 13:40:12 -0500

    idk what happened there but its the same

--- main.py
+++ main.py
@@ -1,19 +1,49 @@
+class URLs:
+    def __init__(self):
+        self.links = {}
+        self.count = 0
+
+    def shorten(self,URL):
+        url =  "https://www.myLink.com/short" + str(self.count)
+        self.count+=1
+        return url
+
+    def store(self,URL):
+        self.links[URL] = self.shorten(URL)
+
+    def printShorten(self,URL):
+        print(self.links[URL])
+
+    def printURLs(self):
+        print(self.links)
+
+    def getCount(self):
+        return self.count
+    
+    def findShortened(self, search):
+        for value in self.links.values():
+            if value == search:
+                return self.links.key(value)
 
 
 state = "0"
 
+url = URLs()
+
 while(state != "00"):
-    print("Would you like to: \n\nStore a URl (0), \nStore and return a shortened URL (1), \nlist stored URLs (2), \nDisplay number of stored URLs (3), \nReturn a full URl from a shortened input (4) \n\n")
+    print("Would you like to: \n\nStore a URl (0), \nStore and return a shortened URL (1), \nlist stored URLs (2), \nDisplay number of stored URLs (3), \nReturn a full URL from a shortened input (4) \n\n")
     state = input()
     if(state == "0"):
-        break
+        link = input("Please input your link: ")
+        url.store(link)
     if(state == "1"):
-        break
+        link = input("Please input your link: ")
+        url.store(link)
+        url.printShorten(link)
     if(state == "2"):
-        break
+        url.printURLs()
     if(state == "3"):
-        break
+        print(url.getCount())
     if(state == "4"):
-        break
-    break
+        url.findShortened("https://www.myLink.com/short0")
 
