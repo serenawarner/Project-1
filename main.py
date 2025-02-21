14783df711f857cf4eb7f52fbc4856609ae6c4a7
Serena <swarner26@wooster.edu>
2025-02-21 13:52:34 -0500

    changes

--- main.py
+++ main.py
@@ -25,7 +25,10 @@ class URLs:
             if val == search:
                 return key
 
-    def validate(URL):
+    def validate(self,URL):
+        print(URL[0:8].lower())
+        if(URL[0:8].lower() != "https://"):
+            return False
         return True
 
 
@@ -34,17 +37,21 @@ state = "0"
 url = URLs()
 
 while(state != "00"):
-    print("Would you like to: \n   (0) Store a URl\n   (1) Store and return a shortened URL\n   (2) List stored URLs\n   (3) Display number of stored URLs\n   (4) Return a full URL from a shortened input\n")
+    print("Would you like to: \n   (0) Store a URL\n   (1) Store and return a shortened URL\n   (2) List stored URLs\n   (3) Display number of stored URLs\n   (4) Return a full URL from a shortened input\n")
     state = input()
     if(state == "0"):
         link = input("Please input your link: ")
         if(url.validate(link)):
             url.store(link)
+        else:
+            print("Invalid link")
     if(state == "1"):
         link = input("Please input your link: ")
         if(url.validate(link)):
             url.store(link)
             url.printShorten(link)
+        else:
+            print("Invalid link")
     if(state == "2"):
         url.printURLs()
     if(state == "3"):
