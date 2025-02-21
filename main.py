228d8cf2b3cfca29da6bf8ecf1e584e9fb7dffcf
critterday <tjons26@wooster.edu>
2025-02-21 13:52:47 -0500

    gang i broke the code im gonna be fr i dont know how to fix this

--- main.py
+++ main.py
@@ -5,17 +5,20 @@ state = "0"
 url = URLs()
 
 while(state != "00"):
-    print("Would you like to: \n\nStore a URl (0), \nStore and return a shortened URL (1), \nlist stored URLs (2), \nDisplay number of stored URLs (3), \nReturn a full URl from a shortened input (4) \n\n")
+    print("Would you like to: \n\nStore a URl (0), \nStore and return a shortened URL (1), \nlist stored URLs (2), \nDisplay number of stored URLs (3), \nReturn a full URL from a shortened input (4) \n\n")
     state = input()
     if(state == "0"):
-        break
+        link = input("Please input your link: ")
+        url.store(link)
     if(state == "1"):
-        break
+        link = input("Please input your link: ")
+        url.store(link)
+        url.printShorten(link)
     if(state == "2"):
         url.printURLs()
     if(state == "3"):
         print(url.getCount())
     if(state == "4"):
-        break
-    break
+        url.findShortened("https://www.myLink.com/short0")
 
+ 
